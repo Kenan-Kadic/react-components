a259ad44a7e7ca676ccf2fe92b840085afbb484a
Kenan Kadic <ken.kadic@outlook.com>
2021-12-02 14:14:50 -0600

    Approvalcard.js added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import Approvalcard from "./Approvalcard";
 
 if (module.hot) {
   module.hot.accept();
@@ -9,9 +11,25 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-    <CommentDetail />
-    <CommentDetail />
-    <CommentDetail />
+        <Approvalcard>
+            <div>
+            <h4>Warning!</h4>
+            Are you sure you want to do this?
+            </div>
+        </Approvalcard>
+
+        <Approvalcard>
+            <CommentDetail author= "Kenan" timeAgo="Today at 4.45pm" blogText="I love your post" imageProp = {faker.image.image()} />
+        </Approvalcard>
+
+        <Approvalcard>
+            <CommentDetail author= "Alex" timeAgo="Today at 2pm" blogText="I like it a lot too" imageProp= {faker.image.image()} /> 
+        </Approvalcard>
+
+        <Approvalcard>
+            <CommentDetail author= "Jane" timeAgo="Yesterday at 5pm" blogText="You can't say no to that" imageProp= {faker.image.image()}/>
+        </Approvalcard>
+
     </div>
   );
 };
