4277156eff87c8ff2c86ce3a17b1b291f9bce18a
Kenan Kadic <ken.kadic@outlook.com>
2021-12-02 10:06:43 -0600

    CommentDetail.js added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from './CommentDetail';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+    <CommentDetail />
+    <CommentDetail />
+    <CommentDetail />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
